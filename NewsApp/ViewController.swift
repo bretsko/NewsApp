8e06ae0ff1e41042d2166158117792df8a446f95
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-23 21:22:12 -0400

    [add] initial commiit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Macbook Pro 15 on 4/23/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
