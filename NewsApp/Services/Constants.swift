e4d3f253302aa69dbf267f37c4da5aab0bb23f66
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-23 22:18:37 -0400

    [add] removed unneeded constants

--- NewsApp/Services/Constants.swift
+++ NewsApp/Services/Constants.swift
@@ -8,68 +8,6 @@
 
 import UIKit
 
-let kAPPNAME: String = "Medicine Subscription"
-
-let ibuprofen = Product(name: "Ibuprofen", description: "OTC Pills", image: kPILLIMAGE)
-let aspirin = Product(name: "Aspirin", description: "OTC Pills", image: kPILLIMAGE)
-let excedrin = Product(name: "Excedrin", description: "OTC Pills", image: kPILLIMAGE)
-let acetaminophen = Product(name: "Acetaminophen", description: "OTC Pills", image: kPILLIMAGE)
-let propanolol = Product(name: "Propanolol", description: "RX Pills", image: kPILLIMAGE)
-let migramax = Product(name: "Migramax", description: "RX Pills", image: kPILLIMAGE)
-let toradol = Product(name: "Toradol", description: "RX Pills", image: kPILLIMAGE)
-let headBand = Product(name: "Head Band", description: "Migraine Gadgets", image: kHEADBANDIMAGE)
-let mouthGuard = Product(name: "Toradol", description: "Migraine Gadgets", image: kMOUTHGUARDIMAGE)
-let headMassager = Product(name: "Head Massager", description: "Migraine Gadgets", image: kHEADMASSAGERIMAGE)
-let eyeGlasses = Product(name: "Light Sensitive Glasses", description: "Migraine Gadgets", image: kEYEGLASSESIMAGE)
-let oil1 = Product(name: "Lavander Oil", description: "Oils", image: kOILIMAGE)
-let oil2 = Product(name: "Peppermint Oil", description: "Oils", image: kOILIMAGE)
-let tea1 = Product(name: "Chamomile Tea", description: "Tea", image: kTEAIMAGE)
-let tea2 = Product(name: "Peppermint Tea", description: "Tea", image: kTEAIMAGE)
-let pregnant1 = Product(name: "Back Brace", description: "Pregnant Support", image: kPREGNANTIMAGE)
-let pregnant2 = Product(name: "Pillow", description: "Pregnant Support", image: kPREGNANTIMAGE)
-let kSAMPLEPRODUCT: Product = Product(name: "Advil", description: "OTC Pills", image: kPILLIMAGE)
-let kSAMPLEPRODUCTS: [Product] = [acetaminophen, ibuprofen, excedrin, aspirin, propanolol, migramax, toradol, headBand, mouthGuard, headMassager, eyeGlasses, oil1, oil2, tea1, tea2, pregnant1, pregnant2]
-
-//images
-let kMAKESCHOOLIMAGE: UIImage = UIImage(named: "makeschoolLogo")!
-let kAPPLOGO: UIImage = UIImage(named: "appLogo")!
-let kAPPLOGOCLEAR: UIImage = UIImage(named: "clearAppLogo")!
-let kSTARIMAGE: UIImage = UIImage(named: "star")!
-let kSTARFILLEDIMAGE: UIImage = UIImage(named: "starFilled")!
-let kMIGRAINEIMAGE: UIImage = UIImage(named: "migraine")!
-let kSETTINGSIMAGE: UIImage = UIImage(named: "settings")!
-let kHOMEIMAGE: UIImage = UIImage(named: "home")!
-let kNEWIMAGE: UIImage = UIImage(named: "new")!
-let kHISTORYIMAGE: UIImage = UIImage(named: "history")!
-let kPROFILEIMAGE: UIImage = UIImage(named: "profile")!
-let kBACKBUTTONIMAGE: UIImage = UIImage(named: "backButton")!
-
-//Box Categories
-let kMEDICATIONSIMAGE: UIImage = UIImage(named: "medications")!
-let kOINTMENTSIMAGE: UIImage = UIImage(named: "ointments")!
-let kSPECIALBOXESIMAGE: UIImage = UIImage(named: "specialBoxes")!
-let kSUPPLIESIMAGE: UIImage = UIImage(named: "supplies")!
-
-let kPILLIMAGE: UIImage = UIImage(named: "pill")!
-
-let kCREAMIMAGE: UIImage = UIImage(named: "cream")!
-let kOILIMAGE: UIImage = UIImage(named: "oil")!
-let kBATHIMAGE: UIImage = UIImage(named: "bath")!
-
-let kEYEMASKIMAGE: UIImage = UIImage(named: "eyeMask")!
-let kHEADBANDIMAGE: UIImage = UIImage(named: "headBand")!
-let kEARPLUGIMAGE: UIImage = UIImage(named: "earPlug")!
-let kEYEGLASSESIMAGE: UIImage = UIImage(named: "eyeGlasses")!
-let kHEADMASSAGERIMAGE: UIImage = UIImage(named: "headMassager")!
-let kMOUTHGUARDIMAGE: UIImage = UIImage(named: "mouthGuard")!
-
-let kTEAIMAGE: UIImage = UIImage(named: "tea")!
-let kPREGNANTIMAGE: UIImage = UIImage(named: "pregnant")!
-let kECONOMYBOXIMAGE: UIImage = UIImage(named: "economyBox")!
-let kPREMIUMBOXIMAGE: UIImage = UIImage(named: "premiumBox")!
-let kDELUXEBOXIMAGE: UIImage = UIImage(named: "deluxeBox")!
-let kBOXOFTHEDAYIMAGE: UIImage = UIImage(named: "boxOfTheDay")!
-
 //color
 let kMAINCOLOR: UIColor = UIColor(hexString: "#6a4bc9", alpha: 1.0)
 let kOFFWHITECOLOR: UIColor = UIColor(hexString: "#f0f0f0")
