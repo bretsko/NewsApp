98db359c23905563b297dc18648a3e4c30662928
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-24 16:05:39 -0400

    [add] made tableView work

--- NewsApp/Controllers/NewsListVC.swift
+++ NewsApp/Controllers/NewsListVC.swift
@@ -12,9 +12,10 @@ class NewsListVC: UIViewController, Storyboarded {
     
 //MARK: Properties
     weak var coordinator: MainCoordinator?
+    var newsList: [News] = []
     
 //MARK: Views
-    
+    @IBOutlet weak var tableView: UITableView!
     
 //MARK: App LifeCycle
     override func viewDidLoad() {
@@ -25,7 +26,14 @@ class NewsListVC: UIViewController, Storyboarded {
 
 //MARK: Private Methods
     fileprivate func setupViews() {
-        
+        self.title = "News List"
+        setupTableView()
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(NewsCell.self, forCellReuseIdentifier: String(describing: NewsCell.self))
     }
     
 //MARK: IBActions
@@ -36,4 +44,19 @@ class NewsListVC: UIViewController, Storyboarded {
 }
 
 //MARK: Extensions
+extension NewsListVC: UITableViewDelegate {
+    
+}
 
+extension NewsListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: NewsCell = tableView.dequeueReusableCell(withIdentifier: String(describing: NewsCell.self), for: indexPath) as! NewsCell
+        let news = newsList[indexPath.row]
+        cell.titleLabel.text = "\(news.title)"
+        return cell
+    }
+}
