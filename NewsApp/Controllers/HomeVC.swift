35a4bb287e079ccec9de3956e4a9d54d6e6dd230
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-24 16:05:31 -0400

    [add] made collectionView works

--- NewsApp/Controllers/HomeVC.swift
+++ NewsApp/Controllers/HomeVC.swift
@@ -12,8 +12,10 @@ class HomeVC: UIViewController, Storyboarded {
     
 //MARK: Properties
     weak var coordinator: MainCoordinator?
+    var categories: [Category] = []
     
 //MARK: Views
+    @IBOutlet weak var collectionView: UICollectionView!
     
     
 //MARK: App LifeCycle
@@ -25,7 +27,24 @@ class HomeVC: UIViewController, Storyboarded {
 
 //MARK: Private Methods
     fileprivate func setupViews() {
-        self.view.backgroundColor = .red
+        self.title = "NewsStand"
+        setupCollectionView()
+        addDummyData()
+    }
+    
+    fileprivate func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: String(describing: CategoryCell.self))
+    }
+    
+    fileprivate func addDummyData() {
+        categories = [
+            Category(name: "Business", news: [News(title: "Stocks up", body: "Stocks up"), News(title: "Stocks down", body: "Stocks down"), ]),
+            Category(name: "Health", news: [News(title: "Health up", body: "Health up"), News(title: "Health down", body: "Health down"), ]),
+            Category(name: "Economy", news: [News(title: "Economy up", body: "Economy up"), News(title: "Economy down", body: "Economy down"), ]),
+        ]
+        collectionView.reloadData()
     }
     
 //MARK: IBActions
@@ -36,3 +55,21 @@ class HomeVC: UIViewController, Storyboarded {
 }
 
 //MARK: Extensions
+extension HomeVC: UICollectionViewDelegate {
+    
+}
+
+extension HomeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: CategoryCell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CategoryCell.self), for: indexPath) as! CategoryCell
+        let category = categories[indexPath.row]
+        cell.backgroundColor = .red
+        cell.populateViews(category: category)
+//        cell.lbl.text = "\(category.name)"
+        return cell
+    }
+}
