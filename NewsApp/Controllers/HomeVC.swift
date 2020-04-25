7f5530cd36abc3e786cd866101591fdfeeceab81
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-24 21:08:06 -0400

    [add] made cell work by unregistering cell

--- NewsApp/Controllers/HomeVC.swift
+++ NewsApp/Controllers/HomeVC.swift
@@ -35,7 +35,8 @@ class HomeVC: UIViewController, Storyboarded {
     fileprivate func setupCollectionView() {
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: String(describing: CategoryCell.self))
+        collectionView.collectionViewLayout = CategoryFlowLayout()
+//        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: String(describing: CategoryCell.self)) //no need since it's inside collectionView's story
     }
     
     fileprivate func addDummyData() {
@@ -43,6 +44,12 @@ class HomeVC: UIViewController, Storyboarded {
             Category(name: "Business", news: [News(title: "Stocks up", body: "Stocks up"), News(title: "Stocks down", body: "Stocks down"), ]),
             Category(name: "Health", news: [News(title: "Health up", body: "Health up"), News(title: "Health down", body: "Health down"), ]),
             Category(name: "Economy", news: [News(title: "Economy up", body: "Economy up"), News(title: "Economy down", body: "Economy down"), ]),
+            Category(name: "Business2", news: [News(title: "Stocks up", body: "Stocks up"), News(title: "Stocks down", body: "Stocks down"), ]),
+            Category(name: "Health2", news: [News(title: "Health up", body: "Health up"), News(title: "Health down", body: "Health down"), ]),
+            Category(name: "Economy2", news: [News(title: "Economy up", body: "Economy up"), News(title: "Economy down", body: "Economy down"), ]),
+            Category(name: "Business3", news: [News(title: "Stocks up", body: "Stocks up"), News(title: "Stocks down", body: "Stocks down"), ]),
+            Category(name: "Health3", news: [News(title: "Health up", body: "Health up"), News(title: "Health down", body: "Health down"), ]),
+            Category(name: "Economy3", news: [News(title: "Economy up", body: "Economy up"), News(title: "Economy down", body: "Economy down"), ]),
         ]
         collectionView.reloadData()
     }
@@ -55,9 +62,13 @@ class HomeVC: UIViewController, Storyboarded {
 }
 
 //MARK: Extensions
-extension HomeVC: UICollectionViewDelegate {
-    
-}
+extension HomeVC: UICollectionViewDelegate {}
+
+//extension HomeVC: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: self.view.safeAreaLayoutGuide.layoutFrame.width / 2 - 24, height: self.view.safeAreaLayoutGuide.layoutFrame.height / 3 - 30)
+//    }
+//}
 
 extension HomeVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -67,9 +78,8 @@ extension HomeVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: CategoryCell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CategoryCell.self), for: indexPath) as! CategoryCell
         let category = categories[indexPath.row]
-        cell.backgroundColor = .red
-        cell.populateViews(category: category)
-//        cell.lbl.text = "\(category.name)"
+        cell.category = category
+        cell.backgroundColor = .gray
         return cell
     }
 }
