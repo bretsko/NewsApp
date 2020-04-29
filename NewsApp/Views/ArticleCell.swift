6d98d999a19ecd9130f6ca63b28b4091835470d1
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-28 21:12:59 -0400

    [add] a way to print loading and hide imageView if there is no image fetched

--- NewsApp/Views/ArticleCell.swift
+++ NewsApp/Views/ArticleCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ArticleCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
@@ -23,13 +24,22 @@ class ArticleCell: UITableViewCell {
     }
     
     func populateViews(article: Article) {
+        imgView.image = UIImage()
         mainIndicator.shouldAnimate(shouldAnimate: false)
         guard let title = article.title else { return }
         titleLabel.text = title
-        guard let imgUrl = article.urlToImage else {
-            imgIndicator.shouldAnimate(shouldAnimate: false)
-            imgView.isHidden = true
-            return
-        }
+        imgView.kf.setImage(with: article.urlToImage, placeholder: UIImage(),
+                            progressBlock: { receivedSize, totalSize in
+//                                print("Progress=\(receivedSize)/\(totalSize)")
+                            },
+                            completionHandler: { result in
+                                self.imgIndicator.shouldAnimate(shouldAnimate: false)
+                                do {
+                                    let _ = try result.get() //value
+                                } catch {
+                                    self.imgView.isHidden = true
+                                    print("Article Image Error=", error.localizedDescription)
+                                }
+                            })
     }
 }
