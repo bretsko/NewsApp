76ec797365ba11d9dbe2b2962d8ae331e8d7226d
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-02 14:48:59 -0400

    [add] comments and made them optional

--- NewsApp/Models/Article.swift
+++ NewsApp/Models/Article.swift
@@ -9,15 +9,11 @@
 import UIKit
 
 public struct ArticleList: Codable {
-    let status: String //"ok" or error
-    var code: String? //code and message will have value if status is error
-    var message: String? //if status is error
-    var totalResults: Int? //if status is ok
+    public let status: String? //"ok" or error
+    public let code: String? //code and message will have value if status is error
+    public let message: String? //if status is error
+    public let totalResults: Int? //if status is ok
     public let articles: [Article]?
-    
-//    enum CodingKeys: String, CodingKey {
-//        case requestStatus = "status", articles
-//    }
 }
 
 public struct Source: Codable {
@@ -28,13 +24,13 @@ public struct Source: Codable {
 /// A product retrieved from the Product Hunt API.
 public struct Article {
     // Various properties of a post that we either need or want to display
-    public let source: Source
+    public let source: Source?
     public let author: String?
     public let title: String?
     public let description: String?
-    public let publishedAt: String
+    public let publishedAt: String?
     public let content: String? //must be optional because some articles dont have content
-    public let url: URL
+    public let url: URL?
     public let urlToImage: URL?
     
 //    func fetchImage(completion: @escaping (_ image: UIImage?, _ error: String?) -> Void) {
