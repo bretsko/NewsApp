c83f72dd82bdc560565236f9fbfba260893f2e99
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-08 19:19:48 -0400

    [add] all languages

--- NewsApp/Models/Language.swift
+++ NewsApp/Models/Language.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+enum Language: String {
+    case ar = "Arabic", de = "German", en = "English", es = "Spanish", fr = "French", he = "Hebrew", it = "Italian", nl = "Dutch", no = "Norwegian", pt = "Portugal", ru = "Russian", se = "Northern Sami", zh = "Chinese"
+}
+
+extension Language: CaseIterable { //to be able to use Category.allCases
+}
