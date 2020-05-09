b1cfb2c63f985d165c275944a3e4586a9a607198
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-08 23:18:44 -0400

    [add] images to category

--- NewsApp/Models/Category.swift
+++ NewsApp/Models/Category.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 enum Category: String {
     case business = "Business", entertainment = "Entertainment", general = "General", health = "Health", science = "Science", sports = "Sports", technology = "Technology"
+    var image: UIImage {
+        return UIImage(named: "\(String(describing: self)).jpg")!
+    }
 }
 
 extension Category: CaseIterable { //to be able to use Category.allCases
