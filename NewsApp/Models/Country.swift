cd8e7b1980e4f8b0e6c6a77d0d10ff16e90e7827
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-08 21:26:18 -0400

    [add] renamed some countries

--- NewsApp/Models/Country.swift
+++ NewsApp/Models/Country.swift
@@ -13,13 +13,13 @@ enum Country: String {
     be = "Belgium", bg = "Bulgaria", br = "Brazil",
     ca = "Canada", ch = "Chile", cn = "China", co = "Colombia", cu = "Cuba", cz = "Czechia",
     de = "Denmark", eg = "Egypt", fr = "France",
-    gb = "United Kingdom of Great Britain and Northern Ireland", gr = "Greece",
-    hk = "HongKong", hu = "Hungary", id = "Indonesia", ie = "Ireland", il = "Israel", `in` = "India", it = "Italy",
+    gb = "United Kingdom", gr = "Greece",
+    hk = "Hong Kong", hu = "Hungary", id = "Indonesia", ie = "Ireland", il = "Israel", `in` = "India", it = "Italy",
     jp = "Japan", kr = "Korea", lt = "Lithuania", lv = "Latvia", ma = "Morocco", mx = "Mexico", my = "Malaysia",
     ng = "Nigeria", nl = "Netherlands", no = "Norway", nz = "New Zealand",
-    ph = "Philippines", pl = "Poland", pt = "Portugal", ro = "Romania", rs = "Serbia", ru = "Russia Federation",
+    ph = "Philippines", pl = "Poland", pt = "Portugal", ro = "Romania", rs = "Serbia", ru = "Russia",
     sa = "Saudi Arabia", se = "Sweden", sg = "Singapore", si = "Slovenia", sk = "Slovakia",
-    th = "Thailand", tr = "Turkey", tw = "Taiwan", ua = "Ukraine", us = "United States of America",
+    th = "Thailand", tr = "Turkey", tw = "Taiwan", ua = "Ukraine", us = "U.S.A.",
     ve = "Venezuela", za = "South Africa"
 }
 
