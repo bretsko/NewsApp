bd9e10ae4124523f2b5a3befac19c2bbbfa6a7c1
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-09 01:54:11 -0400

    [add] removed default values for sources

--- NewsApp/Models/Endpoint.swift
+++ NewsApp/Models/Endpoint.swift
@@ -58,8 +58,8 @@ enum EndPoints {
         switch self {
         case .source:
             return [ //find more info at https://newsapi.org/docs/endpoints/sources
-                kCATEGORY: parameters[kCATEGORY] ?? "general", //either: business, entertainment, general, health, science, sports, technology
-                kLANGUAGE: parameters[kLANGUAGE] ?? "en", //Find sources that display news in a specific language. Possible options: ar de en es fr he it nl no pt ru se ud zh . Default: all languages.
+                kCATEGORY: parameters[kCATEGORY] ?? "", //either: business, entertainment, general, health, science, sports, technology
+                kLANGUAGE: parameters[kLANGUAGE] ?? "", //Find sources that display news in a specific language. Possible options: ar de en es fr he it nl no pt ru se ud zh . Default: all languages.
                 kCOUNTRY: parameters[kCOUNTRY] ?? "", //Find sources that display news in a specific country. Possible options: ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za . Default: all countries.
             ]
         case .articles, .language: // /everything has these parameters
