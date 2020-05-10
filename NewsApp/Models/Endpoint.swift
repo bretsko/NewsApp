38581864d57236d7131b321a1e1e11f3f773a4de
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-10 03:47:21 -0400

    [add] sortby, from, and to to other endpoints

--- NewsApp/Models/Endpoint.swift
+++ NewsApp/Models/Endpoint.swift
@@ -82,16 +82,23 @@ enum EndPoints {
                 kCOUNTRY: parameters[kCOUNTRY] ?? "us", //The 2-letter ISO 3166-1 code of the country you want to get headlines for. Possible options: ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za . Note: you can't mix this param with the sources param.
                 kCATEGORY: parameters[kCATEGORY] ??  "general", //The category you want to get headlines for. Possible options: business entertainment general health science sports technology . Note: you can't mix this param with the sources param.
 //                kSOURCES: parameters[kSOURCES] ?? "", //A comma-seperated string of identifiers for the news sources or blogs you want headlines from. Use the /sources endpoint to locate these programmatically or look at the sources index. Note: you can't mix this param with the country or category params.
+//                kFROM: parameters[kFROM] ?? "\(Service.getIso8601DateByWeek(weekCount: -2))", //A date and optional time for the oldest article allowed. This should be in ISO 8601 format (e.g. 2020-04-25 or 2020-04-25T02:36:43) Default: the oldest according to your plan.
+                kFROM: parameters[kFROM] ?? "\(Service.getIso8601DateByWeek(weekCount: -4))", //A date and optional time for the oldest article allowed. This should be in ISO 8601 format (e.g. 2020-04-25 or 2020-04-25T02:36:43) Default: the oldest according to your plan.
+                kTO: parameters[kTO] ?? "",
+                kSORTBY: parameters[kSORTBY] ?? "popularity",
                 kQ: parameters[kQ] ?? "", //Keywords or a phrase to search for.
                 kPAGESIZE: parameters[kPAGESIZE] ??  "20", //The number of results to return per page (request). 20 is the default, 100 is the maximum.
                 kPAGE: parameters[kPAGE] ?? "1", //Use this to page through the results if the total results found is greater than the page size.
             ]
         case .source: //source cannot have country or category endpoint
             return [
-            kSOURCES: parameters[kSOURCES] ?? "", //A comma-seperated string of identifiers for the news sources or blogs you want headlines from. Use the /sources endpoint to locate these programmatically or look at the sources index. Note: you can't mix this param with the country or category params.
-            kQ: parameters[kQ] ?? "", //Keywords or a phrase to search for.
-            kPAGESIZE: parameters[kPAGESIZE] ??  "20", //The number of results to return per page (request). 20 is the default, 100 is the maximum.
-            kPAGE: parameters[kPAGE] ?? "1", //Use this to page through the results if the total results found is greater than the page size.
+                kSOURCES: parameters[kSOURCES] ?? "", //A comma-seperated string of identifiers for the news sources or blogs you want headlines from. Use the /sources endpoint to locate these programmatically or look at the sources index. Note: you can't mix this param with the country or category params.
+                kQ: parameters[kQ] ?? "", //Keywords or a phrase to search for.
+                kFROM: parameters[kFROM] ?? "\(Service.getIso8601DateByWeek(weekCount: -2))", //A date and optional time for the oldest article allowed. This should be in ISO 8601 format (e.g. 2020-04-25 or 2020-04-25T02:36:43) Default: the oldest according to your plan.
+                kTO: parameters[kTO] ?? "",
+                kSORTBY: parameters[kSORTBY] ?? "popularity",
+                kPAGESIZE: parameters[kPAGESIZE] ??  "20", //The number of results to return per page (request). 20 is the default, 100 is the maximum.
+                kPAGE: parameters[kPAGE] ?? "1", //Use this to page through the results if the total results found is greater than the page size.
             ]
         }
     }
