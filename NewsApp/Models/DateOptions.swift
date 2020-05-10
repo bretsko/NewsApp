5daa58954b265ac1bbad238b461b88e3a123acd4
Samuel Folledo <samuelfolledo@gmail.com>
2020-05-10 03:47:35 -0400

    [add] date options for dates

--- NewsApp/Models/DateOptions.swift
+++ NewsApp/Models/DateOptions.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum DateOptions: String {
+    case now = "Now", hours24 = "24 hours", days3 = "3 days", week = "Week", weeks2 = "2 weeks", month = "Month", months3 = "3 months"
+    
+    var asDateParameter: String {
+        switch self {
+        case .now:
+            return ""
+        case .hours24:
+            return Service.getIso8601DateByDay(dayCount: 1)
+        case .days3:
+            return Service.getIso8601DateByDay(dayCount: 3)
+        case .week:
+            return Service.getIso8601DateByWeek(weekCount: 1)
+        case .weeks2:
+            return Service.getIso8601DateByWeek(weekCount: 2)
+        case .month:
+            return Service.getIso8601DateByWeek(weekCount: 4)
+        case .months3:
+            return Service.getIso8601DateByWeek(weekCount: 12)
+        
+        }
+    }
+}
+
+extension DateOptions: CaseIterable { //to be able to use Category.allCases
+    static var fromAllCases: [DateOptions] {
+        return [.hours24, .days3, .week, .weeks2, .month, .months3] //not include now
+    }
+}
+
